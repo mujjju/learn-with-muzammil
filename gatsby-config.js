b747975ec8a0cccd0a7b9e34c1e27fc67635fc99
mujjju <95684118+mujjju@users.noreply.github.com>
2023-03-14 22:11:08 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,5 @@ module.exports = {
     title: `learn-with-muzammo;`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag"]
+  plugins: ["gatsby-plugin-google-gtag","gatsby-plugin-image"]
 };
