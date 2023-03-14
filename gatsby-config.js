92d99cfe7c7f6c10f3aac227c388fa6a80249a93
unknown <muzammilansari865@gmail.com>
2023-03-14 19:11:39 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `learn-with-muzammo;`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag"]
+};
